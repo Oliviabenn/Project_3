3ee822d908f9d2222a2d8d5fc653cc4e4e43b701
ob <oliviabennington@gmail.com>
2021-12-09 17:09:32 -0500

    js for random images

--- mermaid.js
+++ mermaid.js
@@ -1,17 +1,17 @@
-//how to apply this to one button?
-window.addEventListener("beforeunload", function (e) {
-    var confirmationMessage = 'It looks like you have been editing something. '
-                            + 'If you leave before saving, your changes will be lost.';
-
-    (e || window.event).returnValue = confirmationMessage; //Gecko + IE
-    return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
-});
+// //how to apply this to one button?
+// window.addEventListener("beforeunload", function (e) {
+//     var confirmationMessage = 'It looks like you have been editing something. '
+//                             + 'If you leave before saving, your changes will be lost.';
+//
+//     (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+//     return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+// });
 
 //randomly cycle through images
 window.onload = choosePic;
 
 var theAd = 0;
-var adImages = new Array("./images/poke.jpg","./images/fishy.jpg","./images/snacks.jpg","./images/shrimps.jpg","./images/tacos.jpg");
+var adImages = new Array("https://ibb.co/cT86yNq","https://ibb.co/TwPKcJq", "https://ibb.co/n7Hb1tD", "https://ibb.co/jrkyy7H", "https://ibb.co/v15tSsF");
 
 function choosePic() {
      theAd = Math.floor(Math.random() * adImages.length);
@@ -30,7 +30,7 @@ function rotate() {
      setTimeout(rotate, 3 * 1000);
 }
 
-//scroll by 200px when h1 is onclick
-function scrollWin() {
-  window.scrollBy(0, 600);//changed 200px to 600px
-}
+// //scroll by 200px when h1 is onclick
+// function scrollWin() {
+//   window.scrollBy(0, 600);//changed 200px to 600px
+// }
